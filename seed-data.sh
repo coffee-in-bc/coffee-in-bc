bbdaafa084ac4fc4e334ed6d1825d77e7079214b
nqd <dinh.nguyen@veriksystems.com>
2018-05-01 11:19:49 +0700

    seed more data

--- seed-data.sh
+++ seed-data.sh
@@ -43,8 +43,8 @@ GROWER_001=$(curl -X POST \
 -d '{
   "$class": "org.coffeechain.Grower",
   "growerId": "g001",
-  "firstName": "Tyf",
-  "lastName": "Tyl",
+  "firstName": "Ty Farm",
+  "lastName": " ",
   "farmName": "Ty Farm Co.",
   "farmAltitude": 1500,
   "farmRegion": "Dalat",
@@ -61,9 +61,9 @@ GROWER_002=$(curl -X POST \
 -d '{
   "$class": "org.coffeechain.Grower",
   "growerId": "g002",
-  "firstName": "Teof",
-  "lastName": "Teol",
-  "farmName": "Teo Co.",
+  "firstName": "Teo Farm",
+  "lastName": " ",
+  "farmName": "Teo Farm Co.",
   "farmAltitude": 1000,
   "farmRegion": "Buon Me Thuoc",
   "coffeeTypes": ["Arabica","Robusta"]
@@ -117,7 +117,7 @@ REQ001=$(curl -X POST \
 -d '{
   "$class": "org.coffeechain.Request",
   "requestId": "req001",
-  "coffeeType": "string",
+  "coffeeType": "Arabica",
   "quantityInKg": 10,
   "maxPrice": 250000,
   "region": "Dalat",
@@ -127,3 +127,55 @@ REQ001=$(curl -X POST \
 'http://localhost:3000/api/org.coffeechain.Request'
 )
 echo $REQ001
+
+echo "Create second request"
+REQ002=$(curl -X POST \
+--header 'Content-Type: application/json' \
+--header 'Accept: application/json' \
+-d '{
+  "$class": "org.coffeechain.Request",
+  "requestId": "req002",
+  "coffeeType": "Mixed",
+  "quantityInKg": 20,
+  "maxPrice": 250000,
+  "region": "NA",
+  "dateToReceive": "2018-04-30T05:12:01.340Z",
+  "buyer": "b001"
+}' \
+'http://localhost:3000/api/org.coffeechain.Request'
+)
+echo $REQ002
+
+
+#
+# Give offer to first request
+# 
+echo "Create first request"
+OFFER001=$(curl -X POST \
+--header 'Content-Type: application/json' \
+--header 'Accept: application/json' \
+-d '{
+  "$class": "org.coffeechain.Offer",
+  "offerId": "offer001",
+  "price": 249000,
+  "accepted": "false",
+  "request": "req001",
+  "grower": "g001"
+}' \
+'http://localhost:3000/api/org.coffeechain.Offer'
+)
+echo $OFFER001
+
+# Accept the first offer
+
+ACCEPTOFFER001=$(curl -X POST \
+--header 'Content-Type: application/json' \
+--header 'Accept: application/json' \
+-d '{
+  "$class": "org.coffeechain.AcceptOffer",
+  "offer": "offer001",
+  "timestamp": "2018-05-01T02:48:01.912Z"
+}' \
+'http://localhost:3000/api/org.coffeechain.AcceptOffer'
+)
+echo $OFFER001
